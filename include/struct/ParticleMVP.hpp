9ba55fc217b238b8f1e9a1259eaf6aee9806be83
florianvazelle <ponythugflorian@gmail.com>
2021-07-07 11:27:19 +0200

    f

--- include/struct/ParticleMVP.hpp
+++ include/struct/ParticleMVP.hpp
@@ -4,12 +4,12 @@
 #include <Struct/MVP.hpp>
 #include <glm/glm.hpp>
 
-namespace vkl {
+namespace vkm {
 
   struct ParticleMVP : public MVP {
     glm::vec2 screenDim;
   };
 
-}  // namespace vkl
+}  // namespace vkm
 
 #endif  // PARTICLEMVP_HPP
